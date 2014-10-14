fecb54108e10f0d1477d17c7e735efa3b44c9830
khromov <stanislav.khromov@gmail.com>
2014-10-15 01:06:53 +0200

    When development mode constant is not set, assume it's the production environment

--- custom-fields.php
+++ custom-fields.php
@@ -3,7 +3,7 @@
 Plugin Name: Custom Fields
 Plugin URI:
 Description: Provides additional fields
-Version: 2014.04.24
+Version: 2014.10.15
 Author: khromov
 Author URI: http://profiles.wordpress.org/khromov/
 License: GPL2
@@ -11,7 +11,8 @@ License: GPL2
 
 add_action('plugins_loaded', function()
 {
-	if(defined('CUSTOM_CPT_DEVELOPMENT_MODE') && CUSTOM_CPT_DEVELOPMENT_MODE !== true)
+	/* If development mode is not defined in config, load our CPT:s because we assume it's a regular user installing this on a clean site */
+	if(!defined('CUSTOM_CPT_DEVELOPMENT_MODE') || (defined('CUSTOM_CPT_DEVELOPMENT_MODE') && (CUSTOM_CPT_DEVELOPMENT_MODE !== true)))
 	{
 		foreach (glob(__DIR__ . "/field_groups/*.php") as $filename)
 			include $filename;
